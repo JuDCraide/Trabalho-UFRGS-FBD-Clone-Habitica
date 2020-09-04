182502ea1c53f33172ef6d3af020d831f6bfaa61
Leonardo R. Gobatto <lrgobatto@gmail.com>
2020-09-03 22:57:11 -0300

    Integrando a listagem de atividades com a API

--- mobile/src/pages/Atividades/index.js
+++ mobile/src/pages/Atividades/index.js
@@ -10,13 +10,94 @@ import ItemTarefa from '../../components/ItemTarefa';
 import avatarImg from '../../assets/avatar.png';
 import styles from './styles';
 
+import api from '../../utils/api'
+import { getId } from '../../utils/authentication';
+
 export default function Atividades(props) {
 
-    const health = 45;
-    const xp = (570 * 100) / 770;
+    const [health, setHealth] = useState(0)
+    const [xp, setXp] = useState(0)
+    const [nivel, setNivel] = useState(0)
+    const [moedas, setMoedas] = useState(0)
+    const [classe, setClasse] = useState('');
+
+    const [habitos, setHabitos] = useState([]);
+    const [rotinas, setRotinas] = useState([]);
+    const [tarefas, setTarefas] = useState([]);
+
+    useEffect(() => {
+
+        async function loadDados() {
+            try {
+                const response = await api.get("/usuario/1");
+                let dados = response.data;
+                setHealth(dados.saude);
+                setXp(dados.experiencia)
+                setNivel(dados.experiencia / 100)
+                setMoedas(dados.moedas)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        async function loadClasse() {
+            try {
+                const response = await api.get("/usuario/1/classe");
+                let dados = response.data;
+                setClasse(response.data.classe)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        async function loadHabitos() {
+
+            let id = await getId();
+
+            try {
+                const response = await api.get(`/usuario/${id}/habito`);
+                setHabitos(response.data);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        async function loadRotinas() {
+
+            let id = await getId();
+
+            try {
+                const response = await api.get(`/usuario/${id}/rotina`);
+                setRotinas(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        async function loadTarefas() {
+            let id = await getId();
+            try {
+                const response = await api.get(`/usuario/${id}/tarefa`);
+                setTarefas(response.data);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        loadClasse()
+        loadDados()
+        loadHabitos()
+        loadRotinas()
+        loadTarefas()
+    }, []);
+
+
+
+    const xpbar = (xp) / 770 * 100;
     return (
         <SafeAreaView style={styles.container}>
-            <Header titulo={props.route.name} {...props}/>
+            <Header titulo={props.route.name} {...props} />
             <View style={styles.usuario}>
                 <View style={{ ...styles.linha, height: 100 }}>
                     <Image source={avatarImg} style={styles.img} />
@@ -26,30 +107,37 @@ export default function Atividades(props) {
                                 <View style={{ ...styles.porcentagem, backgroundColor: "#ff6165", width: `${health}%` }}></View>
                             </View>
                             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <Text style={styles.textos}>45/100</Text>
+                                <Text style={styles.textos}>{health}/100</Text>
                                 <Text style={styles.textos}>Saúde</Text>
                             </View>
                         </View>
                         <View style={{ flex: 1 }}>
                             <View style={styles.porcentagem}>
-                                <View style={{ ...styles.porcentagem, backgroundColor: "#ffbe5d", width: `${xp}%` }}></View>
+                                <View style={{ ...styles.porcentagem, backgroundColor: "#ffbe5d", width: `${xpbar}%` }}></View>
                             </View>
                             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <Text style={styles.textos}>570/770</Text>
+                                <Text style={styles.textos}>{xp}/770</Text>
                                 <Text style={styles.textos}>Experiência</Text>
                             </View>
                         </View>
                     </View>
                 </View>
                 <View style={styles.linha}>
-                    <Text style={styles.textos}>Lvl 100 Guerreiro</Text>
-                    <Text style={styles.dinheiro}>25 Dinheiro</Text>
+                    <Text style={styles.textos}>Lvl {nivel} {classe}</Text>
+                    <Text style={styles.dinheiro}>{moedas} Dinheiro</Text>
                 </View>
             </View>
             <View style={styles.container}>
-                <ItemHabito nome='Arrumar a cama'/>
-                <ItemRotina nome='Banho'/>
-                <ItemTarefa nome='Trabalho FDB' data="29/08/2020"/>
+                {habitos.map((habito) => {
+                    return (<ItemHabito key={habito.id} nome={habito.nome} positivo={habito.eh_positivo} />)
+
+                })}
+                {rotinas.map((rotina) => {
+                    return (<ItemRotina key={rotina.id} nome={rotina.nome} />)
+                })}
+                {tarefas.map((tarefa) => {
+                    return (<ItemTarefa key={tarefa.id} nome={tarefa.nome} data={tarefa.data_entrega} />)
+                })}
             </View>
         </SafeAreaView>
     );
